f35a98f83067d32e3e3943fee8d8b905bd59acf2
Commandeur Nicolas <comcomnico@gmail.com>
2022-01-11 08:45:35 +0100

    peut pas compiler pcq import datamocklib pas trouvée :3

--- src/exercice1/MainExercice1.java
+++ src/exercice1/MainExercice1.java
@@ -1,21 +1,32 @@
 package exercice1;
 
 import datamocklib.Person;
+import datamocklib.TxtHelper;
 
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class MainExercice1 {
 
     // Format du fichier : firstName,lastName,birthDate,gender,cityOfResidence,cityOfBirth
     // Pas de header
-    public List<Person> parse(List<String> data) {
+    public static List<Person> parse(List<String> data) {
         // A COMPLETER
-        return new ArrayList<Person>();
+        ArrayList<Person> pList = new ArrayList<Person>();
+        pList.add(new Person(data.get(0), data.get(1), data.get(2), data.get(3), data.get(4), data.get(5)));
+        return pList;
     }
 
     public static void main(String[] args) {
         // EXO 1
+        System.out.println(args);
+        List<String> data = TxtHelper.getDataFromTxt(args[0]);
+
+        List<Person> pList = parse(data);
+
+        System.out.println(pList);
+
         /*
          * - Récuperez la liste des personnes sous la forme -> firstName,lastName,birthDate,gender,cityOfResidence,cityOfBirth
          *   grâce à la fonction TxtHelper.getDataFromTxt("persons.txt")
